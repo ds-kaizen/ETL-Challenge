a74fdaed3e4e1b0de1ad33a0914e1d54b6d5d2b6
Aashish Anand Jha <AashishJ@ad.crisil.com>
2022-06-23 20:25:37 +0530

    Committing changes to Master

---
+++ src/README.md
@@ -0,0 +1,86 @@
+# DSR - ETL Challenge
+
+### ETL challenge for Data Engineers.
+
+## Introduction
+The Premier League, also known as the English Premier League or the EPL is the top level of the English football league system. Contested by 20 clubs, it operates on a system of promotion and relegation with the English Football League (EFL).
+The EPL has information of seasons between 09-10 and 18-19. 
+
+## Format and history of the Premier League
+The Premier League is the top tier of England's football pyramid, with 20 teams battling it out for the honour of being crowned English champions.
+
+Home to some of the most famous clubs, players, managers and stadiums in world football, the Premier League is the most-watched league on the planet with one billion homes watching the action in 188 countries.
+
+The league takes place between August and May and involves the teams playing each other home and away across the season, a total of 380 matches.
+
+Three points are awarded for a win, one point for a draw and none for a defeat, with the team with the most points at the end of the season winning the Premier League title.
+
+If any clubs finish with the same number of points, their position in the Premier League table is determined by goal difference, then the number of goals scored. If the teams still cannot be separated, they will be awarded the same position in the table.
+
+The teams that finish in the bottom three of the league table at the end of the campaign are relegated to the Championship, the second tier of English football.
+
+Those teams are replaced by three clubs promoted from the Championship; the sides that finish in first and second place and the third via the end-of-season playoffs.
+
+## About the Challenge
+You are hired as the Data Engineer to process and stage the raw data for all seasons.
+
+You need to create an ETL job that prepares the data for the EPL Research team. Please consider the following:
+    The Job must be an ETL code developed in Python.
+    A deployment for the code is required. Please prefer using open-source tools.
+
+Show the overall architecture of your pipeline. Include all the steps of the workflow.
+
+We want a deployment for the ETL, please remember that best deployments are the easiest to execute.
+
+## Data Dictionary
+The EPL data has season information from 2009-10 to 18-19
+
+Data Dictionary
+
+Abbreviation -- Description
+
+Div           --   League Division\
+Date 	      --  Match Date (dd/mm/yy)\
+HomeTeam 	  --   Home Team\
+AwayTeam 	  --   Away Team\
+FTHG 	      --   Full Time Home Team Goals\
+FTAG 	      --   Full Time Away Team Goals\
+FTR 	      --   Full Time Result (H|Home Win, D|Draw, A|Away Win)\
+HTHG 	      --   Half Time Home Team Goals\
+HTAG 	      --   Half Time Away Team Goals\
+HTR 	      --   Half Time Result (H|Home Win, D|Draw, A|Away Win)\
+Attendance 	  --   Crowd Attendance\
+Referee 	  --   Match Referee\
+HS 	          --   Home Team Shots\
+AS 	          --   Away Team Shots\
+HST 	      --   Home Team Shots on Target\
+AST 	      --   Away Team Shots on Target\
+HHW 	      --   Home Team Hit Woodwork\
+AHW 	      --   Away Team Hit Woodwork\
+HC 	          --   Home Team Corners\
+AC 	          --   Away Team Corners\
+HF 	          --   Home Team Fouls Committed\
+AF 	          --   Away Team Fouls Committed\
+HO 	          --   Home Team Offsides\
+AO 	          --   Away Team Offsides\
+HY 	          --   Home Team Yellow Cards\
+AY 	          --   Away Team Yellow Cards\
+HR 	          --   Home Team Red Cards\
+AR 	          --   Away Team Red Cards\
+HBP 	      --   Home Team Bookings Points (10 | yellow, 25 | red)\
+ABP 	      --   Away Team Bookings Points (10 | yellow, 25 | red)\
+
+## Overall Architechture and Process flow
+
+The ETL process broadly goes thru three stages:
+1. Extract
+2. Transform
+3. Load
+
+we will follow the same appraoch in our job development process. Detailing out each step:
+
+### Extract
+1. we have already been provided with EPL season data in json file. Each file contains the complete information about the season's game.
+2. we will write Python code to iterate thru each file and create a DataFrame.
+3. There are many columns which are not required for our ETL process. we will remove them from our DataFrame.
+4. we will check the datatype of each columns and date format of date column and see if it require any fixing.
---
+++ src/etl_challange.py
@@ -0,0 +1,246 @@
+# importing necessary packages
+from os.path import basename
+import pandas as pd
+import numpy as np
+import glob
+import logging
+
+import yaml
+
+with open("C:/Users/aashishj/Documents/jefferies/etl-challenge-v1-dsr/src/config.yaml", "r") as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+# import config parameter
+basepath = config['basepath']
+inbound = config['inbound']
+outbound = config['outbound']
+log = config['log']
+valid_col_ls = config['valid_col_ls']
+col_rename = config['col_rename']
+col_seq = config['col_seq']
+
+
+class epl_league():
+
+    def __init__(self, file_list):
+        self.file_list = file_list
+
+
+    def read_input_json_file(self, file_list):
+        """this method will read all the input json file from input 'data' folder"""
+
+        epl_df = []
+        for f in file_list:
+            # extracting season information from input file name
+            season = basename(f).removesuffix('.json')
+            json = pd.read_json(f)
+            json['Season'] = season
+            epl_df.append(json)
+        epl_df = pd.concat(epl_df)
+        # reset index to avoid duplicate index operation
+        epl_df = epl_df.reset_index()
+
+        return epl_df
+
+
+    def drop_bad_cols(self, epl_df):
+        """this method will drop all the columns which is not required for job processing"""
+
+        epl_df = epl_df[valid_col_ls]
+        
+        return epl_df
+
+
+    def rename_cols(self, epl_df):
+        """renaming columns to make it more readable"""
+
+        epl_df.rename(columns = col_rename, inplace=True)
+
+        return epl_df
+
+
+    def points(self, epl_df):
+        """ add columns which will keep records of point won or lost by a team"""
+
+        for i, recs in epl_df.iterrows():
+            if recs['FullTimeResult'] == 'H':
+                epl_df.loc[i, 'home_wins'] = 1
+                epl_df.loc[i, 'home_points'] = 3
+            if recs['FullTimeResult'] == 'A':
+                epl_df.loc[i, 'away_wins'] = 1
+                epl_df.loc[i, 'away_points'] = 3
+            if recs['FullTimeResult'] == 'A':
+                epl_df.loc[i, 'home_loss'] = 1
+            if recs['FullTimeResult'] == 'H':
+                epl_df.loc[i, 'away_loss'] = 1
+            if recs['FullTimeResult'] == 'D':
+                epl_df.loc[i, 'draw'] = 1
+
+        return epl_df
+            
+
+
+    def home_team(self, epl_df):
+        """this method will create df for home team"""
+
+        groupby_cols = ['HomeTeam', 'Season', 'LeagueDivision']
+
+        epl_df_home = epl_df.groupby(groupby_cols)\
+            .agg({'home_wins' : 'count', 'home_loss' : 'count', 'home_points' : 'sum', 'draw' : 'count',\
+                'FullTimeResult' : 'count', 'FullTimeHomeTeamGoals' : 'sum', 'FullTimeAwayTeamGoals': 'sum'})\
+                .reset_index()\
+                    .rename(columns={'FullTimeResult' : 'total_home_matches', 'FullTimeHomeTeamGoals' : 'goals_for_home',\
+                        'FullTimeAwayTeamGoals' : 'goals_against_home'})
+
+        return epl_df_home
+
+
+    def away_team(self, epl_df):
+        """this method will create df for away team"""
+
+        groupby_cols = ['AwayTeam', 'Season', 'LeagueDivision']
+
+        epl_df_away = epl_df.groupby(groupby_cols)\
+            .agg({'away_wins' : 'count', 'away_loss' : 'count', 'away_points' : 'sum', 'draw' : 'count',\
+                'FullTimeResult' : 'count', 'FullTimeAwayTeamGoals' : 'sum', 'FullTimeHomeTeamGoals': 'sum'})\
+                .reset_index()\
+                    .rename(columns={'FullTimeResult' : 'total_away_matches', 'FullTimeAwayTeamGoals' : 'goals_for_away',\
+                        'FullTimeHomeTeamGoals' : 'goals_against_away'})
+
+        return epl_df_away        
+
+
+    def league_table(self, epl_df_home, epl_df_away):
+        """this method will concat home_team dataframe with away_team dataframe to create EPL position table"""
+
+        # to concat away_team and home_team, first we need to rename columns.
+        col_rename_home = {'HomeTeam' : 'club', 'total_home_matches' : 'matches_played', 'home_wins' : 'wins',\
+            'home_loss' : 'loss', 'goals_for_home' : 'goals_for', 'goals_against_home' : 'goals_against',\
+                'home_points' : 'points', 'Season' : 'season'}
+
+        epl_df_home = epl_df_home.rename(columns = col_rename_home)
+
+        col_rename_away = {'AwayTeam' : 'club', 'total_away_matches' : 'matches_played', 'away_wins' : 'wins',\
+            'away_loss' : 'loss', 'goals_for_away' : 'goals_for', 'goals_against_away' : 'goals_against',\
+                'away_points' : 'points', 'Season' : 'season'}
+
+        epl_df_away = epl_df_away.rename(columns = col_rename_away)
+
+        league_table = pd.concat([epl_df_home, epl_df_away], axis=0)
+
+        # grouping league table recs to produce desired output
+        groupby_cols = ['club', 'LeagueDivision', 'season']
+
+        league_table = league_table.groupby(groupby_cols)\
+            .agg({'matches_played' : 'sum', 'wins' : 'sum', 'loss': 'sum', 'draw' : 'sum', 'goals_for' : 'sum',\
+                'goals_against' : 'sum', 'points' : 'sum'})\
+                    .reset_index()
+
+        # adding draw points to 'points' column
+        league_table['points'] = league_table['draw'].astype(int)+league_table['points'].astype(int)
+
+        #calculating goal difference
+        league_table['goal_difference'] = league_table['goals_for'] - league_table['goals_against']
+
+        # drop column 'LeagueDivision'
+        league_table = league_table.drop('LeagueDivision', axis=1)
+
+        # sorting league table
+        league_table['goal_difference_abs'] = league_table['goal_difference'].abs()
+        league_table['goal_difference_le'] = league_table['goal_difference'].le(0)
+        league_table = league_table.sort_values(['season', 'points', 'goal_difference_le', 'goal_difference_abs'], ascending=[False, False, True, True])\
+            .drop(['goal_difference_le', 'goal_difference_abs'], axis=1)
+
+        #arranging column sequence
+        league_table = league_table[col_seq]
+
+        return league_table
+
+
+    def best_scoring_team_by_season(self, league_table):
+        """this method will create dataframe for best scoring team by season"""
+        champion_table = league_table.loc[league_table.groupby(['season'])["points"].idxmax()].sort_values(['season'], ascending=False)
+
+        return champion_table
+
+
+
+    def write_excel(self, league_table, champion_table):
+
+        writer_league = pd.ExcelWriter(basepath+outbound+"/EPL_League.xlsx", mode="w", engine="xlsxwriter", date_format='m/d/yyyy', datetime_format='m/d/yyyy')
+        writer_champ = pd.ExcelWriter(basepath+outbound+"/Best_Scoring_Team.xlsx", mode="w", engine="xlsxwriter", date_format='m/d/yyyy', datetime_format='m/d/yyyy')
+
+        row = 0
+        sheet_league = 'league_table'
+        sheet_champ = 'top_team_by_season'   
+
+        league_table.to_excel(writer_league, sheet_name=sheet_league, index=False, startrow=row)
+
+        champion_table.to_excel(writer_champ, sheet_name=sheet_champ, index=False, startrow=row)
+
+        writer_league.save()
+        writer_champ.save()
+
+
+    def main(self):
+        """this method will be the starting point ETL process"""
+        try:
+            epl_df = self.read_input_json_file(file_list)
+
+            epl_df = self.drop_bad_cols(epl_df)
+
+            epl_df = self.rename_cols(epl_df)
+
+            epl_df = self.points(epl_df)
+
+            epl_df_home = self.home_team(epl_df)
+
+            epl_df_away = self.away_team(epl_df)
+
+            league_table = self.league_table(epl_df_home, epl_df_away)
+
+            champion_table = self.best_scoring_team_by_season(league_table)
+
+            self.write_excel(league_table, champion_table)
+
+        except Exception as e:
+            logging.error("Exception occured", exc_info=True)
+            logger.info(f"ETL Process Failed, fix the issue and rerun!! \n")
+
+
+def logs():
+
+    # create log filename
+    filenm=basepath+log+"/"+ "epl_league" + ".log"
+
+    logging.basicConfig(filename=filenm,
+                        format='%(levelname)s - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', 
+                        filemode='w')
+
+    # Creating an object
+    logger = logging.getLogger()
+
+    # Setting the threshold of logger to DEBUG
+    logger.setLevel(logging.DEBUG)
+    
+    return logger
+
+
+if __name__ == "__main__":
+
+    filepath = basepath+inbound+"/*.json"
+    file_list = glob.glob(filepath)
+
+    #initialising logger
+    logger = logs()
+    logger.info("*************************EPL ETL Challange program started************************* \n")
+
+    p = epl_league(file_list)
+    p.main()
+
+    logger.info("*************************EPL ETL Challange program completed successfully************************* \n")
+
+
